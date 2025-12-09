f2caf1952e98bc5fec5204e7c50c5e80827677da
Nikita Obozhin <obozhin.nikita@yandex.ru>
2025-12-09 15:01:51 +0500

    Add passenger_wsgi.py for hosting

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,10 +2,17 @@
 
 import os, sys
 
+# Добавляем путь к проекту
 sys.path.insert(0, '/var/www/u3351782/data/www/le-lawe.ru')
 
+# Добавляем путь к виртуальному окружению
 sys.path.insert(1, '/var/www/u3351782/data/www/le-lawe.ru/venv/lib/python3.10/site-packages')
 
+# Загружаем переменные окружения из .env
+from dotenv import load_dotenv
+load_dotenv('/var/www/u3351782/data/www/le-lawe.ru/.env')
+
+# Устанавливаем модуль настроек Django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'education_portal.settings'
 
 from django.core.wsgi import get_wsgi_application
